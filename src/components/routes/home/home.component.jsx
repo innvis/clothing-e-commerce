d49b3bf72ffc029f51ef642cc3e94254d5335077
DenaldM <dmetuku@gmail.com>
2023-05-10 16:53:16 +0200

    navigation setup

--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Directory from "../../directory/directory-component";
 
 const Home = () => {
@@ -29,6 +30,11 @@ const Home = () => {
     },
   ];
 
-  return <Directory categories={categories} />;
+  return (
+    <div>
+      <Outlet />
+      <Directory categories={categories} />
+    </div>
+  );
 };
 export default Home;
