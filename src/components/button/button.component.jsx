981e5f4e425a59aef0d7ce97035fed3f980a5749
DenaldM <dmetuku@gmail.com>
2023-05-24 18:22:55 +0200

    adding final touches on styling

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,8 @@
-import { BaseButton, GoogleSignButton, InvertedButton } from "./button.styles";
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "./button.styles";
 
 export const BUTTON_TYPE_CLASSES = {
   base: "base",
@@ -9,7 +13,7 @@ export const BUTTON_TYPE_CLASSES = {
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
   ({
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
-    [BUTTON_TYPE_CLASSES.google]: GoogleSignButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
